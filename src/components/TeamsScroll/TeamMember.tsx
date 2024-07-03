6b59a2db4f49cda2e4ac8bd598738a9cbb1919fc
Suraj Hemnani <98706142+Surajh09@users.noreply.github.com>
2024-07-03 22:18:23 +0530

    minor changes

--- src/components/TeamsScroll/TeamMember.tsx
+++ src/components/TeamsScroll/TeamMember.tsx
@@ -1,18 +1,24 @@
 import React from "react";
-import Image from "next/image";
+import Link from "next/link";
 
-const TeamMember = ({ name, role, image }) => {
+const TeamMember = ({ name, role, image, link }) => {
   return (
-    <div className="flex transform flex-col items-center p-4 transition-transform hover:scale-105">
-      <Image
-        className="h-32 w-32 rounded-full shadow-lg"
-        src={image}
-        alt={`${name}'s picture`}
-        width={3000}
-        height={3000}
-      />
-      <h3 className="mt-4 text-lg font-semibold">{name}</h3>
-      <p className="text-gray-500">{role}</p>
+    <div className="mx-4 my-8 transform overflow-hidden rounded-xl bg-gray-400/20 shadow-lg shadow-gray-400 backdrop-blur transition duration-300 ease-in-out hover:scale-105">
+      <Link href={link}>
+        <img
+          className="h-56 w-full object-cover object-center"
+          src={image}
+          alt={name}
+        />
+        <div className="p-4">
+          <h3 className="mb-2 text-lg font-bold">{name}</h3>
+          <p className="mb-2 text-gray-200">{role}</p>
+          <p className="text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            imperdiet bibendum.
+          </p>
+        </div>
+      </Link>
     </div>
   );
 };
--- src/components/TeamsScroll/TeamSection.tsx
+++ src/components/TeamsScroll/TeamSection.tsx
@@ -2,38 +2,143 @@ import React from "react";
 import TeamMember from "./TeamMember";
 
 const teamMembers = [
+  // Your team members data
   {
     name: "John Doe",
     role: "CEO",
     image:
-      "https://images.unsplash.com/photo-1603415526960-fb3a72d51ca6?ixlib=rb-1.2.1&auto=format&fit=crop&w=150&q=80",
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/suraj-hemnani-b4623a251/",
   },
   {
     name: "Jane Smith",
     role: "CTO",
     image:
-      "https://images.unsplash.com/photo-1603415526960-fb3a72d51ca6?ixlib=rb-1.2.1&auto=format&fit=crop&w=150&q=80",
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/suraj-hemnani-b4623a251/",
   },
   {
     name: "Mike Johnson",
     role: "Lead Developer",
     image:
-      "https://images.unsplash.com/photo-1603415526960-fb3a72d51ca6?ixlib=rb-1.2.1&auto=format&fit=crop&w=150&q=80",
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/suraj-hemnani-b4623a251/",
+  },
+  {
+    name: "John Doe",
+    role: "CEO",
+    image:
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/suraj-hemnani-b4623a251/",
+  },
+  {
+    name: "Jane Smith",
+    role: "CTO",
+    image:
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/suraj-hemnani-b4623a251/",
+  },
+  {
+    name: "Mike Johnson",
+    role: "Lead Developer",
+    image:
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/suraj-hemnani-b4623a251/",
+  },
+  {
+    name: "John Doe",
+    role: "CEO",
+    image:
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/suraj-hemnani-b4623a251/",
+  },
+  {
+    name: "Jane Smith",
+    role: "CTO",
+    image:
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/suraj-hemnani-b4623a251/",
+  },
+  {
+    name: "Mike Johnson",
+    role: "Lead Developer",
+    image:
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/suraj-hemnani-b4623a251/",
+  },
+  {
+    name: "John Doe",
+    role: "CEO",
+    image:
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/suraj-hemnani-b4623a251/",
+  },
+  {
+    name: "Jane Smith",
+    role: "CTO",
+    image:
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/suraj-hemnani-b4623a251/",
+  },
+  {
+    name: "Mike Johnson",
+    role: "Lead Developer",
+    image:
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/suraj-hemnani-b4623a251/",
+  },
+  {
+    name: "John Doe",
+    role: "CEO",
+    image:
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/suraj-hemnani-b4623a251/",
+  },
+  {
+    name: "Jane Smith",
+    role: "CTO",
+    image:
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/suraj-hemnani-b4623a251/",
+  },
+  {
+    name: "Mike Johnson",
+    role: "Lead Developer",
+    image:
+      "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    link: "https://www.linkedin.com/in/suraj-hemnani-b4623a251/",
   },
 ];
 
 const TeamSection = () => {
+  const firstRowMembers = teamMembers.slice(0, 3);
+  const remainingMembers = teamMembers.slice(3);
+
   return (
     <section className="bg-inherit py-12">
       <div className="container mx-auto px-4">
         <h2 className="mb-8 text-center text-3xl font-bold">Meet Our Team</h2>
-        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3">
-          {teamMembers.map((member, index) => (
+        {/* First row with 3 members */}
+        <div className="grid grid-cols-3 gap-8">
+          {firstRowMembers.map((member, index) => (
             <TeamMember
               key={index}
               name={member.name}
               role={member.role}
               image={member.image}
+              link={member.link}
+            />
+          ))}
+        </div>
+        {/* Subsequent rows with 4 members each */}
+        <div className="mt-8 grid grid-cols-4 gap-8">
+          {remainingMembers.map((member, index) => (
+            <TeamMember
+              key={index}
+              name={member.name}
+              role={member.role}
+              image={member.image}
+              link={member.link}
             />
           ))}
         </div>
